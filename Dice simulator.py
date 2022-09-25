5c3d364df3482e5fa21ec7aa23f77bdbffbf1676
YATISH MANNE <yatish.manne09@gmail.com>
2022-09-25 19:41:20 +0530

    Update Dice simulator.py

--- Dice simulator.py
+++ Dice simulator.py
@@ -6,8 +6,8 @@ def Dice():
     print(f"The number on the dice is: {dice_Number}")
 Dice()
 while(1):
-        reply=input("Do you want to roll the dice again(S/NO)?")
-        if reply=='S':
+        reply=input("Do you want to roll the dice again(s/no)?")
+        if reply=='s':
             Dice()
         else:
             print("Thanks for rolling me,I will be ready to roll whenver you need me.")
